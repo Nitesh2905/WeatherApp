0310e71665821a772a87c3e1dd50f90ea17153ac
Nitesh2905 <nkyadav2905@gmail.com>
2021-02-21 20:25:23 +0530

    Version 1.0.0

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import Moment from 'react-moment';
+import 'moment-timezone';
+import './App.css';
+
+const api = {
+  key: "58e496b32b2922c9ba050d27de7397e6",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+
+  const [search, setSearch]  = useState('')
+  const [weather, setWeather] = useState({})
+  const date = new Date();
+  
+  function findCity(e) {
+    if(e.key === "Enter"){
+      fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+    .then((res) => res.json())
+    .then(result => {
+      setSearch('')
+      setWeather(result)
+      console.log(result);
+    })
+    }
+  }
+
+  return (
+    <div className={(typeof weather.main !== "undefined" ? ((weather.main.temp >= 20) ? "app warm" : "app"): "app" )}>
+     <main>
+       <div className="search-box">
+         <input type="text" placeholder="Your City Name" 
+         onChange= {e => setSearch(e.target.value)} value={search} onKeyPress={findCity} className="search-bar"/>
+       </div>
+      {(typeof weather.main !== "undefined")? (
+        <div>
+           <div className="location-box">
+         <div className="location">{weather.name}, {weather.sys.country}</div>
+         <div className="date"><Moment format='MMMM Do YYYY, h:mm a'>{date}</Moment></div>
+       </div>
+
+       <div className="weather-box">
+         <div className="temp">
+           {Math.round(weather.main.temp)}° C 
+         </div>
+         <div className="weather">{weather.weather[0].main}</div>
+       </div>
+        </div>
+      ): ('')}
+     </main>
+    </div>
+  );
+}
+
+export default App;
